030cc6b99a583880a15377ce03a7a609dbebd4d9
Hipex123 <117768734+Hipex123@users.noreply.github.com>
2025-01-19 19:44:03 +0100

    Added phone connection checking

--- src/server.py
+++ src/server.py
@@ -2,10 +2,16 @@ import socket, re
 
 tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-address = ("localhost", XXXX)
+address = ("XXXX", XXXX)
 tcpSocket.bind(address)
 
 tcpSocket.listen(1)
+
+userCount = 0
+
+users = {
+
+}
 
 while True:
     print("Waiting for connection...")
@@ -13,9 +19,13 @@ while True:
 
     try:
         print("Client connected")
-         
         while True:
-            data = connection.recv(1024)
+            data = connection.recv(8192)
+            if str(data)[2:-1] == "CONNECTED|PH0NE":
+                userCount += 1
+                users[userCount] = client[0]
+                continue
+
             formatedData = str(data)[2:-3]
 
             f1 = re.sub(r"\\\\", r"\\", formatedData)
@@ -23,8 +33,10 @@ while True:
 
             if len(f2) != 0:
                 print(f2)
- 
+
             if not data:
                 break
+    except:
+        connection.close()
     finally:
         connection.close()
